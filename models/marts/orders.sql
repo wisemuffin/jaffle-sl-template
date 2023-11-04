9c46750a7b6ab613c7b376168f41926b17499709
David Griffiths <davidgg777@hotmail.com>
2023-11-04 18:07:57 +1100

    Update orders.sql

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -21,7 +21,7 @@ order_items_summary as (
         sum(supply_cost) as order_cost,
         sum(is_food_item) as count_food_items,
         sum(is_drink_item) as count_drink_items,
-        sum(1) as demo_count
+        sum(2) as demo_count
 
 
     from order_items_table as order_items
